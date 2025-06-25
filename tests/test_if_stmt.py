850166a69977895ede74939f86ad17d00cd6ec0f
yuvi-mittal <yuvimittal2004@gmail.com>
2025-06-25 19:43:16 +0530

    add int8 in tests

--- tests/test_if_stmt.py
+++ tests/test_if_stmt.py
@@ -13,6 +13,14 @@ from .conftest import check_result
 
 
 @pytest.mark.parametrize(
+    "int_type, literal_type",
+    [
+        (astx.Int32, astx.LiteralInt32),
+        (astx.Int16, astx.LiteralInt16),
+        (astx.Int8, astx.LiteralInt8),
+    ],
+)
+@pytest.mark.parametrize(
     "action,expected_file",
     [
         # ("translate", "test_if_stmt.ll"),
@@ -26,18 +34,22 @@ from .conftest import check_result
     ],
 )
 def test_if_else_stmt(
-    action: str, expected_file: str, builder_class: Type[Builder]
+    action: str,
+    expected_file: str,
+    builder_class: Type[Builder],
+    int_type: type,
+    literal_type: type,
 ) -> None:
     """Test an If statement with an else branch."""
     builder = builder_class()
     module = builder.module()
 
     init_a = astx.InlineVariableDeclaration(
-        "a", type_=astx.Int32(), value=astx.LiteralInt32(10)
+        "a", type_=int_type(), value=literal_type(10)
     )
 
     cond = astx.BinaryOp(
-        op_code=">", lhs=astx.Variable("a"), rhs=astx.LiteralInt32(5)
+        op_code=">", lhs=astx.Variable("a"), rhs=literal_type(5)
     )
 
     then_block = astx.Block()
@@ -49,12 +61,12 @@ def test_if_else_stmt(
     if_stmt = astx.IfStmt(condition=cond, then=then_block, else_=else_block)
 
     main_proto = astx.FunctionPrototype(
-        "main", args=astx.Arguments(), return_type=astx.Int32()
+        "main", args=astx.Arguments(), return_type=int_type()
     )
     main_body = astx.Block()
     main_body.append(init_a)
     main_body.append(if_stmt)
-    main_body.append(astx.FunctionReturn(astx.LiteralInt32(0)))
+    main_body.append(astx.FunctionReturn(literal_type(0)))
 
     main_fn = astx.Function(prototype=main_proto, body=main_body)
     module.block.append(main_fn)
@@ -62,6 +74,14 @@ def test_if_else_stmt(
     check_result(action, builder, module, expected_file)
 
 
+@pytest.mark.parametrize(
+    "int_type, literal_type",
+    [
+        (astx.Int32, astx.LiteralInt32),
+        (astx.Int16, astx.LiteralInt16),
+        (astx.Int8, astx.LiteralInt8),
+    ],
+)
 @pytest.mark.parametrize(
     "action,expected_file",
     [
@@ -75,18 +95,22 @@ def test_if_else_stmt(
     ],
 )
 def test_if_only_stmt(
-    action: str, expected_file: str, builder_class: Type[Builder]
+    action: str,
+    expected_file: str,
+    builder_class: Type[Builder],
+    int_type: type,
+    literal_type: type,
 ) -> None:
     """Test an If statement without an else branch."""
     builder = builder_class()
     module = builder.module()
 
     init_a = astx.InlineVariableDeclaration(
-        "a", type_=astx.Int32(), value=astx.LiteralInt32(3)
+        "a", type_=int_type(), value=literal_type(3)
     )
 
     cond = astx.BinaryOp(
-        op_code=">", lhs=astx.Variable("a"), rhs=astx.LiteralInt32(5)
+        op_code=">", lhs=astx.Variable("a"), rhs=literal_type(5)
     )
 
     then_block = astx.Block()
@@ -95,12 +119,12 @@ def test_if_only_stmt(
     if_stmt = astx.IfStmt(condition=cond, then=then_block)
 
     main_proto = astx.FunctionPrototype(
-        "main", args=astx.Arguments(), return_type=astx.Int32()
+        "main", args=astx.Arguments(), return_type=int_type()
     )
     main_body = astx.Block()
     main_body.append(init_a)
     main_body.append(if_stmt)
-    main_body.append(astx.FunctionReturn(astx.LiteralInt32(0)))
+    main_body.append(astx.FunctionReturn(literal_type(0)))
 
     main_fn = astx.Function(prototype=main_proto, body=main_body)
     module.block.append(main_fn)
