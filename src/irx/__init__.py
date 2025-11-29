8b47003de6e4d5a4b333171c7782174780830d6e
Himanshu Garode <himanshu2006garode@gmail.com>
2025-11-29 11:37:10 +0530

    fix: use hybrid version approach with fallback

--- src/irx/__init__.py
+++ src/irx/__init__.py
@@ -1,9 +1,16 @@
 """Top-level package for IRx."""
 
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    """Return the program version."""
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "1.4.0"  # semantic-release
+
+
 __author__ = """Ivan Ogasawara"""
 __email__ = "ivan.ogasawara@gmail.com"
-
-# Dynamically retrieve version from package metadata
-from importlib.metadata import version as _version
-
-__version__ = _version("pyirx")  # semantic-release
+__version__: str = get_version()
