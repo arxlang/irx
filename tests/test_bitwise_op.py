a93500817c5b3e7833ab64d26251e88a09f6527c
AryanBhirud <aryan.bhirud@hotmail.com>
2026-03-21 02:50:38 +0530

    Parameterize operators for test_bitwise_op.py

--- tests/test_bitwise_op.py
+++ tests/test_bitwise_op.py
@@ -11,7 +11,17 @@ from irx.system import PrintExpr
 from .conftest import check_result
 
 @pytest.mark.parametrize(
-    "int_type, literal_type",
+    "op,lhs,rhs,expected",
+    [
+        ("&", 6, 3, lambda a, b: str(a & b)),
+        ("|", 6, 3, lambda a, b: str(a | b)),
+        ("^", 6, 3, lambda a, b: str(a ^ b)),
+        ("<<", 3, 2, lambda a, b: str(a << b)),
+        (">>", 12, 2, lambda a, b: str(a >> b)),
+    ],
+)
+@pytest.mark.parametrize(
+    "int_type,literal_type",
     [
         (astx.Int32, astx.LiteralInt32),
         (astx.Int16, astx.LiteralInt16),
@@ -20,25 +30,23 @@ from .conftest import check_result
     ],
 )
 @pytest.mark.parametrize("builder_class", [LLVMLiteIR])
-def test_bitwise_and(builder_class: type[Builder], int_type: type, literal_type: type) -> None:
-    """
-    title: Test bitwise AND (&) operation on integer literals.
-    parameters:
-      builder_class:
-        type: type[Builder]
-      int_type:
-        type: type
-      literal_type:
-        type: type
-    """
+def test_bitwise_binary_ops(
+    builder_class: type[Builder],
+    int_type: type,
+    literal_type: type,
+    op: str,
+    lhs: int,
+    rhs: int,
+    expected,
+) -> None:
     builder = builder_class()
     module = builder.module()
-
-    expr = astx.BinaryOp("&", literal_type(6), literal_type(3))
+    left = literal_type(lhs)
+    right = literal_type(rhs)
+    expr = astx.BinaryOp(op, left, right)
     decl = astx.VariableDeclaration(
         name="result", type_=int_type(), value=expr, mutability=astx.MutabilityKind.mutable
     )
-
     main_proto = astx.FunctionPrototype(
         name="main", args=astx.Arguments(), return_type=int_type()
     )
@@ -48,26 +56,37 @@ def test_bitwise_and(builder_class: type[Builder], int_type: type, literal_type:
     main_block.append(astx.FunctionReturn(literal_type(0)))
     main_fn = astx.FunctionDef(prototype=main_proto, body=main_block)
     module.block.append(main_fn)
+    check_result("build", builder, module, expected_output=expected(lhs, rhs))
 
-    check_result("build", builder, module, expected_output="2")
-
-
-@pytest.mark.parametrize("int_type, literal_type", [
-    (astx.Int32, astx.LiteralInt32),
-    (astx.Int16, astx.LiteralInt16),
-    (astx.Int8, astx.LiteralInt8),
-    (astx.Int64, astx.LiteralInt64),
-])
+@pytest.mark.parametrize(
+    "val",
+    [15, 0, 1, 255],
+)
+@pytest.mark.parametrize(
+    "int_type,literal_type,bitwidth",
+    [
+        (astx.Int8, astx.LiteralInt8, 8),
+        (astx.Int16, astx.LiteralInt16, 16),
+        (astx.Int32, astx.LiteralInt32, 32),
+        (astx.Int64, astx.LiteralInt64, 64),
+    ],
+)
 @pytest.mark.parametrize("builder_class", [LLVMLiteIR])
-def test_bitwise_or(builder_class: type[Builder], int_type: type, literal_type: type) -> None:
+def test_bitwise_not(
+    builder_class: type[Builder],
+    int_type: type,
+    literal_type: type,
+    bitwidth: int,
+    val: int,
+) -> None:
     builder = builder_class()
     module = builder.module()
-    expr = astx.BinaryOp("|", literal_type(6), literal_type(3))
+    expr = astx.UnaryOp("~", literal_type(val))
     decl = astx.VariableDeclaration(
-      name="result", type_=int_type(), value=expr, mutability=astx.MutabilityKind.mutable
+        name="result", type_=int_type(), value=expr, mutability=astx.MutabilityKind.mutable
     )
     main_proto = astx.FunctionPrototype(
-      name="main", args=astx.Arguments(), return_type=int_type()
+        name="main", args=astx.Arguments(), return_type=int_type()
     )
     main_block = astx.Block()
     main_block.append(decl)
@@ -75,108 +94,12 @@ def test_bitwise_or(builder_class: type[Builder], int_type: type, literal_type: 
     main_block.append(astx.FunctionReturn(literal_type(0)))
     main_fn = astx.FunctionDef(prototype=main_proto, body=main_block)
     module.block.append(main_fn)
-    check_result("build", builder, module, expected_output="7")
-
-
-@pytest.mark.parametrize("int_type, literal_type", [
-    (astx.Int32, astx.LiteralInt32),
-    (astx.Int16, astx.LiteralInt16),
-    (astx.Int8, astx.LiteralInt8),
-    (astx.Int64, astx.LiteralInt64),
-])
-@pytest.mark.parametrize("builder_class", [LLVMLiteIR])
-def test_bitwise_xor(builder_class: type[Builder], int_type: type, literal_type: type) -> None:
-    builder = builder_class()
-    module = builder.module()
-    expr = astx.BinaryOp("^", literal_type(6), literal_type(3))
-    decl = astx.VariableDeclaration(
-      name="result", type_=int_type(), value=expr, mutability=astx.MutabilityKind.mutable
-    )
-    main_proto = astx.FunctionPrototype(
-      name="main", args=astx.Arguments(), return_type=int_type()
-    )
-    main_block = astx.Block()
-    main_block.append(decl)
-    main_block.append(PrintExpr(astx.Identifier("result")))
-    main_block.append(astx.FunctionReturn(literal_type(0)))
-    main_fn = astx.FunctionDef(prototype=main_proto, body=main_block)
-    module.block.append(main_fn)
-    check_result("build", builder, module, expected_output="5")
-
-
-@pytest.mark.parametrize("int_type, literal_type", [
-    (astx.Int32, astx.LiteralInt32),
-    (astx.Int16, astx.LiteralInt16),
-    (astx.Int8, astx.LiteralInt8),
-    (astx.Int64, astx.LiteralInt64),
-])
-@pytest.mark.parametrize("builder_class", [LLVMLiteIR])
-def test_left_shift(builder_class: type[Builder], int_type: type, literal_type: type) -> None:
-    builder = builder_class()
-    module = builder.module()
-    expr = astx.BinaryOp("<<", literal_type(3), literal_type(2))
-    decl = astx.VariableDeclaration(
-      name="result", type_=int_type(), value=expr, mutability=astx.MutabilityKind.mutable
-    )
-    main_proto = astx.FunctionPrototype(
-      name="main", args=astx.Arguments(), return_type=int_type()
-    )
-    main_block = astx.Block()
-    main_block.append(decl)
-    main_block.append(PrintExpr(astx.Identifier("result")))
-    main_block.append(astx.FunctionReturn(literal_type(0)))
-    main_fn = astx.FunctionDef(prototype=main_proto, body=main_block)
-    module.block.append(main_fn)
-    check_result("build", builder, module, expected_output="12")
-
-
-@pytest.mark.parametrize("int_type, literal_type", [
-    (astx.Int32, astx.LiteralInt32),
-    (astx.Int16, astx.LiteralInt16),
-    (astx.Int8, astx.LiteralInt8),
-    (astx.Int64, astx.LiteralInt64),
-])
-@pytest.mark.parametrize("builder_class", [LLVMLiteIR])
-def test_right_shift(builder_class: type[Builder], int_type: type, literal_type: type) -> None:
-    builder = builder_class()
-    module = builder.module()
-    expr = astx.BinaryOp(">>", literal_type(12), literal_type(2))
-    decl = astx.VariableDeclaration(
-      name="result", type_=int_type(), value=expr, mutability=astx.MutabilityKind.mutable
-    )
-    main_proto = astx.FunctionPrototype(
-      name="main", args=astx.Arguments(), return_type=int_type()
-    )
-    main_block = astx.Block()
-    main_block.append(decl)
-    main_block.append(PrintExpr(astx.Identifier("result")))
-    main_block.append(astx.FunctionReturn(literal_type(0)))
-    main_fn = astx.FunctionDef(prototype=main_proto, body=main_block)
-    module.block.append(main_fn)
-    check_result("build", builder, module, expected_output="3")
-
-
-@pytest.mark.parametrize("int_type, literal_type", [
-    (astx.Int32, astx.LiteralInt32),
-    (astx.Int16, astx.LiteralInt16),
-    (astx.Int8, astx.LiteralInt8),
-    (astx.Int64, astx.LiteralInt64),
-])
-@pytest.mark.parametrize("builder_class", [LLVMLiteIR])
-def test_bitwise_not(builder_class: type[Builder], int_type: type, literal_type: type) -> None:
-    builder = builder_class()
-    module = builder.module()
-    expr = astx.UnaryOp("~", literal_type(15))
-    decl = astx.VariableDeclaration(
-      name="result", type_=int_type(), value=expr, mutability=astx.MutabilityKind.mutable
-    )
-    main_proto = astx.FunctionPrototype(
-      name="main", args=astx.Arguments(), return_type=int_type()
-    )
-    main_block = astx.Block()
-    main_block.append(decl)
-    main_block.append(PrintExpr(astx.Identifier("result")))
-    main_block.append(astx.FunctionReturn(literal_type(0)))
-    main_fn = astx.FunctionDef(prototype=main_proto, body=main_block)
-    module.block.append(main_fn)
-    check_result("build", builder, module, expected_output=str(~15))
+    # Compute the expected result as a signed integer of the correct width
+    mask = (1 << bitwidth) - 1
+    unsigned = (~val) & mask
+    sign_bit = 1 << (bitwidth - 1)
+    if unsigned & sign_bit:
+        expected = str(unsigned - (1 << bitwidth))
+    else:
+        expected = str(unsigned)
+    check_result("build", builder, module, expected_output=expected)
